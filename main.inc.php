12bf7ad3988c9208e1e3793be50c082c08e1ef56
Jeremy <jradwan@users.noreply.github.com>
2017-05-02 22:00:47 -0400

    Update main.inc.php

--- main.inc.php
+++ main.inc.php
@@ -8,5 +8,23 @@ Author: windracer (Jeremy C. Radwan)
 Author URI: http://www.windracer.net/blog
 */
 
-defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
-
+if (!defined('PHPWG_ROOT_PATH'))
+{
+  die('Hacking attempt!');
+}
+add_event_handler('tabsheet_before_select','ppm_add_tab', 50, 2);
+function ppm_add_tab($sheets, $id)
+{  
+  load_language('plugin.lang', PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
+  
+  if ($id == 'photo')
+  {
+    $sheets['update'] = array(
+      'caption' => l10n('Move'),
+      'url' => get_root_url().'admin.php?page=plugin-ppm-'.$_GET['image_id'],
+      );
+  }
+  
+  return $sheets;
+}
+?>
