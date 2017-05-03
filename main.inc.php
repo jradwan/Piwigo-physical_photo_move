8b7dea455277fe0ea28cb2b0ca5780e59308a183
Jeremy <jradwan@users.noreply.github.com>
2017-05-02 21:55:49 -0400

    Create main.inc.php

---
+++ main.inc.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: Physical Photo Move
+Version: 0.1
+Description: Move a photo from one physical album to another, preserving all metadata
+Plugin URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: windracer (Jeremy C. Radwan)
+Author URI: http://www.windracer.net/blog
+*/
+
+defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
+
